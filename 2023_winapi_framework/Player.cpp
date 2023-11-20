cf5628adb0ae63ca971c1e2bf99a33921ff18ac2
VsPot07 <cocoacake84@gmail.com>
2023-11-20 14:55:28 +0900

    1120

--- 2023_winapi_framework/Player.cpp
+++ 2023_winapi_framework/Player.cpp
@@ -56,27 +56,27 @@ void Player::Update()
 {
 	Vec2 vPos = GetPos();
 
-	if (KEY_PRESS(KEY_TYPE::A))
+	if (KEY_PRESS(KEY_TYPE::LEFT))
 	{
 		vPos.x -= 100.f * fDT;
 		GetAnimator()->PlayAnim(L"Jiwoo_Left", true);
 	}
-	if (KEY_PRESS(KEY_TYPE::D))
+	if (KEY_PRESS(KEY_TYPE::RIGHT))
 	{
 		vPos.x += 100.f * fDT;
 		GetAnimator()->PlayAnim(L"Jiwoo_Right", true);
 	}
-	if (KEY_PRESS(KEY_TYPE::W))
+	if (KEY_PRESS(KEY_TYPE::UP))
 	{
 		vPos.y -= 100.f * fDT;
 		GetAnimator()->PlayAnim(L"Jiwoo_Back", true);
 	}
-	if (KEY_PRESS(KEY_TYPE::S))
+	if (KEY_PRESS(KEY_TYPE::DOWN))
 	{
 		vPos.y += 100.f * fDT;
 		GetAnimator()->PlayAnim(L"Jiwoo_Front", true);
 	}
-	if (KEY_DOWN(KEY_TYPE::LBUTTON))
+	if (KEY_DOWN(KEY_TYPE::SPACE))
 	{
 		CreateBullet();
 		ResMgr::GetInst()->Play(L"Shoot");
