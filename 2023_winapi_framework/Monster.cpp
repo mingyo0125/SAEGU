640a792146482a49e4271c0680ed5151ce20ea41
VsPot07 <cocoacake84@gmail.com>
2023-11-20 15:40:59 +0900

    1120

--- 2023_winapi_framework/Monster.cpp
+++ 2023_winapi_framework/Monster.cpp
@@ -4,11 +4,11 @@
 #include "Collider.h"
 #include "Object.h"
 #include "EventMgr.h"
+
 Monster::Monster()
 	: m_fSpeed(100.f)
-	, m_fMaxDis(50.f)
+	, m_target()
 	, m_vCenterPos(Vec2(0.f,0.f))
-	, m_fDir(1.f) // 오른쪽부터 이동
 	, m_iHp(5)
 {
 	CreateCollider();
@@ -21,17 +21,13 @@ Monster::~Monster()
 void Monster::Update()
 {
 	Vec2 vCurPos = GetPos();
-	vCurPos.x += m_fSpeed * fDT * m_fDir;
-	
-	// 내가 갈 수 있는 거리 최대로 갔냐? => 방향 바꿔줄거야.
-	float fDist = abs(m_vCenterPos.x - vCurPos.x) - m_fMaxDis;
-	if (fDist > 0.f)
-	{
-		// dir 바꾸기
-		m_fDir *= -1;
-		vCurPos.x += fDist * m_fDir;
-	}
-	SetPos(vCurPos);
+	Vec2 vTargetPos = m_target.GetPos();
+	//Vec2 moveDir = (vTargetPos - vCurPos).Normalize();
+	Vec2 moveDir = (vCurPos - vTargetPos).Normalize();
+
+	vCurPos = vCurPos + (moveDir * 1);
+
+	SetPos(vCurPos);	
 }
 
 void Monster::EnterCollision(Collider* _pOther)
--- 2023_winapi_framework/Start_Scene.cpp
+++ 2023_winapi_framework/Start_Scene.cpp
@@ -9,33 +9,20 @@
 #include "ResMgr.h"
 void Start_Scene::Init()
 {
+	float fMonsterScale = 50;
+
 	Object* pObj = new Player;
 	pObj->SetPos((Vec2({Core::GetInst()->GetResolution().x /2, Core::GetInst()->GetResolution().y / 2})));
 	pObj->SetScale(Vec2(100.f,100.f));
 	AddObject(pObj, OBJECT_GROUP::PLAYER);
 
-	// 몬스터 세팅 마구마구 배치를 해봅시다.
+	// 적 생성
+	Monster* pMonster = new Monster;
+	pMonster->SetPos(Vec2(100, 100));
+	pMonster->SetScale(Vec2(fMonsterScale, fMonsterScale));
+	pMonster->SetCenterPos(pMonster->GetPos());
+	AddObject(pMonster, OBJECT_GROUP::MONSTER);
 
-	Vec2 vResolution = Core::GetInst()->GetResolution();
-	Monster* pMonster = nullptr;
-	int iMonster = 10;		// 몬스터 수 
-	float fMoveDist = 30.f; // 움직일 거리
-	float fMonsterScale = 50.f; // 몬스터 크기
-	// 해상도x - ( 움직일 거리 + 오브젝트 크기 /2) * 2 / 몬스터수 -1 
-	float fTerm = (vResolution.x - (fMoveDist + fMonsterScale / 2.f) * 2) 
-					/ (float)(iMonster -1);
-	for (int i = 0; i < iMonster; ++i)
-	{
-		pMonster = new Monster;
-		pMonster->SetPos(Vec2(
-			(fMoveDist + fMonsterScale / 2.f) + i* fTerm
-			,300.f));
-		pMonster->SetScale(Vec2(fMonsterScale, fMonsterScale));
-		pMonster->SetCenterPos(pMonster->GetPos());
-		pMonster->SetMoveDis(fMoveDist);
-		AddObject(pMonster, OBJECT_GROUP::MONSTER);
-	}
-	// 사운드 세팅
 	ResMgr::GetInst()->LoadSound(L"BGM", L"Sound\\Retro_bgm.wav", true);
 	ResMgr::GetInst()->LoadSound(L"Shoot", L"Sound\\laserShoot.wav", false);
 	ResMgr::GetInst()->Play(L"BGM");
