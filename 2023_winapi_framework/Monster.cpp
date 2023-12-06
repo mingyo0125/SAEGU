3e35d4b7d584150d0bfeb9b3335d562522adc3a9
VsPot07 <cocoacake84@gmail.com>
2023-12-06 09:53:55 +0900

    1206

--- 2023_winapi_framework/Monster.cpp
+++ 2023_winapi_framework/Monster.cpp
@@ -6,16 +6,25 @@
 #include "EventMgr.h"
 #include "ItemSpawner.h"
 #include "KeyMgr.h"
+#include "Animator.h"
+#include "Animation.h"
+#include "ResMgr.h"
 
 bool m_isDie;
 Vec2 m_vCurPos;
 
 Monster::Monster(Object* target, float speed, int hp)
-	: m_target(target), m_fSpeed(speed), m_iHp(hp)
+	: m_target(target), m_fSpeed(speed), m_iHp(hp), m_moveTexture(nullptr)
 {
+	Object::SetName(L"Enemy");
+
 	m_target = target;
 	m_fSpeed = speed;
 	m_iHp = hp;
+
+	m_moveTexture = ResMgr::GetInst()->TexLoad(L"EnemyIdle", L"Texture\\FlyEnemyIdle.bmp");
+	CreateAnimator();
+	GetAnimator()->CreateAnim(L"Enemy_Idle", m_moveTexture, Vec2(30, 20), Vec2(30, 30), Vec2(30, 0), 6, 0.1f);
 
 	CreateCollider();
 }
@@ -33,6 +42,8 @@ void Monster::Update()
 	m_vCurPos = m_vCurPos + (moveDir * m_fSpeed);
 
 	SetPos(m_vCurPos);
+
+	GetAnimator()->PlayAnim(L"Enemy_Idle", false, 1);
 }
 
 void Monster::EnterCollision(Collider* _pOther)
@@ -54,7 +65,7 @@ void Monster::StayCollision(Collider* _pOther)
 
 void Monster::Render(HDC _dc)
 {
-
+	Component_Render(_dc);
 }
 
 void Monster::SetDie()
--- 2023_winapi_framework/Monster.h
+++ 2023_winapi_framework/Monster.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Object.h"
 #include "Player.h"
+#include "Texture.h"
+
 class Monster : public Object
 {
 public:
@@ -22,5 +24,6 @@ private:
 	Vec2 m_vCenterPos; // 중심 위치
 	Object* m_target; // 이동할 수 있는 최대 거리
 	int   m_iHp; // 체력
+	Texture* m_moveTexture;
 };
 
